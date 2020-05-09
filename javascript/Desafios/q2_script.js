230f83e2fd907c36a4446da90749d099c843f5f0
clesiorods <clesiorodrigues96@gmail.com>
2020-05-09 00:35:58 -0300

    Questão 02 do desafio final,concluida

--- javascript/Desafios/q2_script.js
+++ javascript/Desafios/q2_script.js
@@ -2,22 +2,33 @@ var in_user = document.querySelector('input#username');
 var in_botao = document.querySelector('input#botao');
 var dados = document.querySelector('div#dados');
 var img = document.querySelector('img#img');
+var lista = document.querySelector('ul#itens');
 
 in_botao.addEventListener('click', buscar);
 
 
 function buscar() {
-    dados.innerHTML = dados.innerHTML + '<h2> Aguarde... <h2>';
 
-    axios.get('https://api.github.com/users/' + in_user.value)
+    axios.get('https://api.github.com/users/' + in_user.value + '/repos')
         .then((dataRequest) => {
-            img.src = dataRequest.data.avatar_url;
-            console.log(dataRequest.data.avatar_url);
-            dados.innerHTML = '';
-            dados.append(img);
-            dados.append(dataRequest.data.avatar_url);
+
+            lista.innerHTML = '';
+
+            for(rep in dataRequest.data) {
+                var novo_li = document.createElement('li');
+                novo_li.innerHTML = dataRequest.data[rep].name;
+                lista.appendChild(novo_li);
+            }
         })
         .catch((erro) => {
             console.warn('Erro da requisição');
+        })
+    axios.get('https://api.github.com/users/' + in_user.value)
+        .then((receb) => {
+            img.src = receb.data.avatar_url;
+            console.log(receb.data.avatar_url);
+        })
+        .catch((erro) => {
+            console.log('Erro no pedido do avatar');
         })
 }
